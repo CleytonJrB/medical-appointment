67047371ded1b9b04ff46afc4015b9e9399f58d9
CleytonJrB <cleytonjrb@outlook.com>
2025-06-08 23:26:40 -0300

    FIX: new menu data

--- src/constantes/menu-data.js
+++ src/constantes/menu-data.js
@@ -2,22 +2,35 @@ import { routes } from "../utils/general";
 
 export const menuSideBarList = (userType) => {
   if (userType === "ADMIN") {
-    return [];
+    return [
+      {
+        text: "Home",
+        icon: "cottage",
+        link: [routes.protected.dashboard],
+        redirect: routes.protected.dashboard,
+      },
+      {
+        text: "Todos Médicos",
+        icon: "medicalServices",
+        link: [routes.protected.allDoctors],
+        redirect: routes.protected.allDoctors,
+      },
+    ];
   }
 
   if (userType === "DOCTOR") {
     return [
       {
-        text: "Dashboard",
-        icon: "dashboard",
+        text: "Home",
+        icon: "cottage",
         link: [routes.protected.dashboard],
         redirect: routes.protected.dashboard,
       },
       {
-        text: "Detalhes Médicos",
-        icon: "medication",
-        link: [routes.protected.details],
-        redirect: routes.protected.details,
+        text: "Minhas Consultas",
+        icon: "turned",
+        link: [routes.protected.medicalAppointments],
+        redirect: routes.protected.medicalAppointments,
       },
     ];
   }
