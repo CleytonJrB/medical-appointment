8d25b79ca99af7973ede5da8dc01b5e45c4787f1
CleytonJrB <cleytonjrb@outlook.com>
2025-06-08 23:25:56 -0300

    FIX: new Icons

--- src/components/customIcon/CustomIcon.jsx
+++ src/components/customIcon/CustomIcon.jsx
@@ -1,10 +1,13 @@
 import { customColors } from "../../styles/colors";
 
 import DashboardCustomizeRoundedIcon from "@mui/icons-material/DashboardCustomizeRounded";
-import MedicationRoundedIcon from "@mui/icons-material/MedicationRounded";
+import MedicationOutlinedIcon from "@mui/icons-material/MedicationOutlined";
 import CottageOutlinedIcon from "@mui/icons-material/CottageOutlined";
 import MedicalServicesOutlinedIcon from "@mui/icons-material/MedicalServicesOutlined";
 import EventNoteOutlinedIcon from "@mui/icons-material/EventNoteOutlined";
+import TurnedInNotOutlinedIcon from "@mui/icons-material/TurnedInNotOutlined";
+import AnalyticsOutlinedIcon from "@mui/icons-material/AnalyticsOutlined";
+import MedicalInformationOutlinedIcon from "@mui/icons-material/MedicalInformationOutlined";
 
 export default function CustomIcon({
   icon,
@@ -13,12 +16,15 @@ export default function CustomIcon({
 }) {
   const icons = {
     dashboard: <DashboardCustomizeRoundedIcon sx={{ fontSize: size, color }} />,
-    medication: <MedicationRoundedIcon sx={{ fontSize: size, color }} />,
+    medication: <MedicationOutlinedIcon sx={{ fontSize: size, color }} />,
     cottage: <CottageOutlinedIcon sx={{ fontSize: size, color }} />,
     medicalServices: (
       <MedicalServicesOutlinedIcon sx={{ fontSize: size, color }} />
     ),
     eventNote: <EventNoteOutlinedIcon sx={{ fontSize: size, color }} />,
+    turned: <TurnedInNotOutlinedIcon sx={{ fontSize: size, color }} />,
+    analytic: <AnalyticsOutlinedIcon sx={{ fontSize: size, color }} />,
+    medical: <MedicalInformationOutlinedIcon sx={{ fontSize: size, color }} />,
   };
 
   return icons[icon] || <></>;
